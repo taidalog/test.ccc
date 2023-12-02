3b0a501af0aeba0a2b9dbbc528eff316fcdee731
taidalog <72747279+taidalog@users.noreply.github.com>
2023-12-03 03:01:16 +0900

    Implemented help window. Close #4.
    Keyboard shortcuts are available.

--- src/App.fs
+++ src/App.fs
@@ -2,11 +2,20 @@ namespace Ccc
 
 open Browser.Dom
 open Browser.Types
+open Fable.Core
+open Fable.Core.JsInterop
 open Command
 open Timer'
 
 module App =
     let keyboardshortcut (e: KeyboardEvent) =
+        let helpWindow = document.getElementById "helpWindow"
+
+        let isHelpWindowActive =
+            helpWindow.classList
+            |> (fun x -> JS.Constructors.Array?from(x))
+            |> Array.contains "active"
+
         match document.activeElement.id with
         | "commandInput" ->
             match e.key with
@@ -14,6 +23,12 @@ module App =
             | _ -> ()
         | _ ->
             match e.key with
+            | "Escape" ->
+                if isHelpWindowActive then
+                    helpWindow.classList.remove "active"
+            | "?" ->
+                if not isHelpWindowActive then
+                    helpWindow.classList.add "active"
             | "\\" ->
                 (document.getElementById "commandInput").focus ()
                 e.preventDefault ()
@@ -35,6 +50,11 @@ module App =
 
                     commandInput.value |> splitInput' |> Array.map parse |> Array.toList |> start
                     false
+
+            [ "helpButton"; "helpClose" ]
+            |> List.iter (fun x ->
+                (document.getElementById x :?> HTMLButtonElement).onclick <-
+                    fun _ -> (document.getElementById "helpWindow").classList.toggle "active" |> ignore)
 
             document.onkeydown <- fun (e: KeyboardEvent) -> keyboardshortcut e)
     )
