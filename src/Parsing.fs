6cc249009f7e6ea29e8a6643775b9c1ca5c705b5
taidalog <72747279+taidalog@users.noreply.github.com>
2024-06-02 18:27:57 +0900

    Wrote parsers using Fermata.ParserCombinators. #33

---
+++ src/Parsing.fs
@@ -0,0 +1,105 @@
+// ccc Version 0.5.0
+// https://github.com/taidalog/ccc
+// Copyright (c) 2023-2024 taidalog
+// This software is licensed under the MIT License.
+// https://github.com/taidalog/ccc/blob/main/LICENSE
+namespace Ccc
+
+open System
+open Fermata.ParserCombinators.Parsers
+
+module Parsing =
+    type Options =
+        | Color of string
+        | Background of string
+        | Message of string
+
+    type CommandAndOptions =
+        | Up of TimeSpan * Options list
+        | Down of TimeSpan * Options list
+
+    let oneOrMore (p: Parser<'T>) : Parser<'T list> =
+        map' (fun (x, xs) -> x :: xs) (p <&> many p)
+
+    let digit: Parser<char> = [ '0' .. '9' ] |> List.map char' |> List.reduce (<|>)
+
+    let hex: Parser<char> =
+        [ '0' .. '9' ] @ [ 'A' .. 'F' ] @ [ 'a' .. 'f' ]
+        |> List.map char'
+        |> List.reduce (<|>)
+
+    let integer: Parser<int> =
+        let f = List.map string >> String.concat "" >> int
+        map' (fun (x, y) -> x :: y |> f) (digit <&> many digit)
+
+    let integer2: Parser<int> =
+        let f (x, y) = sprintf "%c%c" x y |> int
+        map' f (digit <&> digit)
+
+    let integer1: Parser<int> = map' (string >> int) digit
+
+    let integer2or1: Parser<int> = integer2 <|> integer1
+
+    let spaces: Parser<string> =
+        map' (List.map string >> String.concat "") (oneOrMore (char' ' '))
+
+    let minute: Parser<TimeSpan> =
+        let f (m, s) = TimeSpan(0, m, s)
+        map' f (integer2or1 <+&> string' ":" <&> integer2)
+
+    let hour: Parser<TimeSpan> =
+        let f ((h, m), s) = TimeSpan(h, m, s)
+        map' f (integer2or1 <+&> char' ':' <&> integer2 <+&> char' ':' <&> integer2)
+
+    let time: Parser<TimeSpan> = hour <|> minute
+
+    let hexCode: Parser<string> =
+        let f (h, xs) =
+            sprintf "%c%s" h ((List.map string >> String.concat "") xs)
+
+        map' f (char' '#' <&> (repeat 6 hex))
+
+    let colorOption: Parser<Options> =
+        map'
+            Options.Color
+            ((string' "--color" <|> string' "-c") <&> spaces <&+> hexCode
+             <+&> (pos spaces <|> end'))
+
+    let backgroundOption: Parser<Options> =
+        map'
+            Options.Background
+            ((string' "--background" <|> string' "-bg") <&> spaces <&+> hexCode
+             <+&> (pos spaces <|> end'))
+
+    let messageOption: Parser<Options> =
+        let name = string' "--message" <|> string' "-m"
+
+        let body' =
+            let withoutOptions =
+                (neg (string' "--color")
+                 <&> neg (string' "-c")
+                 <&> neg (string' "--background")
+                 <&> neg (string' "-bg"))
+
+            (many (any <+&> withoutOptions))
+
+        map'
+            (List.map string >> String.concat "" >> (fun x -> x.Trim()) >> Options.Message)
+            (name <&> spaces <&+> body')
+
+    let options: Parser<Options list> =
+        many (
+            (spaces <&+> colorOption)
+            <|> (spaces <&+> backgroundOption)
+            <|> (spaces <&+> messageOption)
+        )
+
+    let downCommand: Parser<CommandAndOptions> =
+        let f ((_, t), l) = CommandAndOptions.Down(t, l)
+        map' f (string' "down" <+&> spaces <&> time <&> options)
+
+    let upCommand: Parser<CommandAndOptions> =
+        let f ((_, t), l) = CommandAndOptions.Up(t, l)
+        map' f (string' "up" <+&> spaces <&> time <&> options)
+
+    let command: Parser<CommandAndOptions> = downCommand <|> upCommand
---
+++ tests/Parsing.Tests.fs
@@ -0,0 +1,283 @@
+// ccc Version 0.5.0
+// https://github.com/taidalog/ccc
+// Copyright (c) 2023-2024 taidalog
+// This software is licensed under the MIT License.
+// https://github.com/taidalog/ccc/blob/main/LICENSE
+module Parsing.Tests
+
+open System
+open Xunit
+open Fermata.ParserCombinators.Parsers
+open Ccc.Parsing
+
+[<Fact>]
+let ``digit 1`` () =
+    let expected = Ok('1', State("1:23:45", 1))
+    let actual = digit (State("1:23:45", 0))
+    Assert.Equal(expected, actual)
+
+[<Fact>]
+let ``hex 1`` () =
+    let expected = Ok('6', State("65a2ac", 1))
+    let actual = hex (State("65a2ac", 0))
+    Assert.Equal(expected, actual)
+
+[<Fact>]
+let ``hex 2`` () =
+    let expected = Ok('a', State("65a2ac", 3))
+    let actual = hex (State("65a2ac", 2))
+    Assert.Equal(expected, actual)
+
+[<Fact>]
+let ``integer 1`` () =
+    let expected = Ok(1, State("1:23:45", 1))
+    let actual = integer (State("1:23:45", 0))
+    Assert.Equal(expected, actual)
+
+[<Fact>]
+let ``integer2 1`` () =
+    let expected = Ok(12, State("12:34", 2))
+    let actual = integer2 (State("12:34", 0))
+    Assert.Equal(expected, actual)
+
+[<Fact>]
+let ``integer1 1`` () =
+    let expected = Ok(1, State("12:34", 1))
+    let actual = integer1 (State("12:34", 0))
+    Assert.Equal(expected, actual)
+
+[<Fact>]
+let ``integer2or1 1`` () =
+    let expected = Ok(12, State("12:34", 2))
+    let actual = integer2or1 (State("12:34", 0))
+    Assert.Equal(expected, actual)
+
+[<Fact>]
+let ``minute 1`` () =
+    let expected = Ok(TimeSpan(0, 12, 34), State("12:34", 5))
+    let actual = minute (State("12:34", 0))
+    Assert.Equal(expected, actual)
+
+[<Fact>]
+let ``hour 1`` () =
+    let expected = Ok(TimeSpan(1, 23, 45), State("1:23:45", 7))
+    let actual = hour (State("1:23:45", 0))
+    Assert.Equal(expected, actual)
+
+[<Fact>]
+let ``time 1`` () =
+    let expected = Ok(TimeSpan(0, 12, 34), State("12:34", 5))
+    let actual = time (State("12:34", 0))
+    Assert.Equal(expected, actual)
+
+[<Fact>]
+let ``time 2`` () =
+    let expected = Ok(TimeSpan(1, 23, 45), State("1:23:45", 7))
+    let actual = time (State("1:23:45", 0))
+    Assert.Equal(expected, actual)
+
+[<Fact>]
+let ``hexCode 1`` () =
+    let expected = Ok("#65a2ac", State("#65a2ac", 7))
+    let actual = hexCode (State("#65a2ac", 0))
+    Assert.Equal(expected, actual)
+
+[<Fact>]
+let ``spaces 1`` () =
+    let expected = Ok(" ", State(" <- a space", 1))
+    let actual = spaces (State(" <- a space", 0))
+    Assert.Equal(expected, actual)
+
+[<Fact>]
+let ``spaces 2`` () =
+    let expected = Ok("  ", State("  <- two spaces", 2))
+    let actual = spaces (State("  <- two spaces", 0))
+    Assert.Equal(expected, actual)
+
+[<Fact>]
+let ``colorOption 1`` () =
+    let expected = Ok(Options.Color "#65a2ac", State("--color #65a2ac", 15))
+    let actual = colorOption (State("--color #65a2ac", 0))
+    Assert.Equal(expected, actual)
+
+[<Fact>]
+let ``colorOption 2`` () =
+    let expected = Ok(Options.Color "#65a2ac", State("-c #65a2ac", 10))
+    let actual = colorOption (State("-c #65a2ac", 0))
+    Assert.Equal(expected, actual)
+
+[<Fact>]
+let ``colorOption 3`` () =
+    let expected = Ok(Options.Color "#65a2ac", State("-c #65a2ac -bg #ffffff", 10))
+    let actual = colorOption (State("-c #65a2ac -bg #ffffff", 0))
+    Assert.Equal(expected, actual)
+
+[<Fact>]
+let ``colorOption 4`` () =
+    let expected = Error("Parsing failed.", State("-c#65a2ac", 0))
+    let actual = colorOption (State("-c#65a2ac", 0))
+    Assert.Equal(expected, actual)
+
+[<Fact>]
+let ``colorOption 5`` () =
+    let expected = Error("Parsing failed.", State("-c -bg #ffffff", 0))
+    let actual = colorOption (State("-c -bg #ffffff", 0))
+    Assert.Equal(expected, actual)
+
+[<Fact>]
+let ``colorOption 6`` () =
+    let expected = Error("Parsing failed.", State("-c #xxxxxx", 0))
+    let actual = colorOption (State("-c #xxxxxx", 0))
+    Assert.Equal(expected, actual)
+
+[<Fact>]
+let ``colorOption 7`` () =
+    let expected = Error("Parsing failed.", State("-c #65a2acff", 0))
+    let actual = colorOption (State("-c #65a2acff", 0))
+    Assert.Equal(expected, actual)
+
+[<Fact>]
+let ``backgroundOption 1`` () =
+    let expected = Ok(Options.Background "#65a2ac", State("--background #65a2ac", 20))
+    let actual = backgroundOption (State("--background #65a2ac", 0))
+    Assert.Equal(expected, actual)
+
+[<Fact>]
+let ``backgroundOption 2`` () =
+    let expected = Ok(Options.Background "#65a2ac", State("-bg #65a2ac", 11))
+    let actual = backgroundOption (State("-bg #65a2ac", 0))
+    Assert.Equal(expected, actual)
+
+[<Fact>]
+let ``backgroundOption 3`` () =
+    let expected = Ok(Options.Background "#65a2ac", State("-bg #65a2ac -c #ffffff", 11))
+    let actual = backgroundOption (State("-bg #65a2ac -c #ffffff", 0))
+    Assert.Equal(expected, actual)
+
+[<Fact>]
+let ``backgroundOption 4`` () =
+    let expected = Error("Parsing failed.", State("-bg#65a2ac", 0))
+    let actual = backgroundOption (State("-bg#65a2ac", 0))
+    Assert.Equal(expected, actual)
+
+[<Fact>]
+let ``backgroundOption 5`` () =
+    let expected = Error("Parsing failed.", State("-bg -c #ffffff", 0))
+    let actual = backgroundOption (State("-bg -c #ffffff", 0))
+    Assert.Equal(expected, actual)
+
+[<Fact>]
+let ``backgroundOption 6`` () =
+    let expected = Error("Parsing failed.", State("-bg #xxxxxx", 0))
+    let actual = backgroundOption (State("-bg #xxxxxx", 0))
+    Assert.Equal(expected, actual)
+
+[<Fact>]
+let ``backgroundOption 7`` () =
+    let expected = Error("Parsing failed.", State("-bg #65a2acff", 0))
+    let actual = backgroundOption (State("-bg #65a2acff", 0))
+    Assert.Equal(expected, actual)
+
+[<Fact>]
+let ``messageOption 1`` () =
+    let expected = Ok(Options.Message "hey he", State("--message hey hey", 16))
+    let actual = messageOption (State("--message hey hey", 0))
+    Assert.Equal(expected, actual)
+
+[<Fact>]
+let ``messageOption 2`` () =
+    let expected =
+        Ok(Options.Message "hey hey", State("--message hey hey --color #65a2ac", 17))
+
+    let actual = messageOption (State("--message hey hey --color #65a2ac", 0))
+    Assert.Equal(expected, actual)
+
+[<Fact>]
+let ``downCommand 1`` () =
+    let expected =
+        Ok(CommandAndOptions.Down(TimeSpan(0, 5, 0), [ Options.Message "he" ]), State("down 5:00 -m hey", 15))
+
+    let actual = downCommand (State("down 5:00 -m hey", 0))
+
+    Assert.Equal(expected, actual)
+
+[<Fact>]
+let ``downCommand 2`` () =
+    let expected =
+        Ok(
+            CommandAndOptions.Down(
+                TimeSpan(0, 5, 0),
+                [ Options.Color "#ffffff"
+                  Options.Background "#65a2ac"
+                  Options.Message "hey he" ]
+            ),
+            State("down 5:00 --color #ffffff --background #65a2ac --message hey hey", 63)
+        )
+
+    let actual =
+        downCommand (State("down 5:00 --color #ffffff --background #65a2ac --message hey hey", 0))
+
+    Assert.Equal(expected, actual)
+
+[<Fact>]
+let ``upCommand 1`` () =
+    let expected =
+        Ok(CommandAndOptions.Up(TimeSpan(0, 5, 0), [ Options.Message "he" ]), State("up 5:00 -m hey", 13))
+
+    let actual = upCommand (State("up 5:00 -m hey", 0))
+
+    Assert.Equal(expected, actual)
+
+[<Fact>]
+let ``upCommand 2`` () =
+    let expected =
+        Ok(
+            CommandAndOptions.Up(
+                TimeSpan(0, 5, 0),
+                [ Options.Color "#ffffff"
+                  Options.Background "#65a2ac"
+                  Options.Message "hey he" ]
+            ),
+            State("up 5:00 --color #ffffff --background #65a2ac --message hey hey", 61)
+        )
+
+    let actual =
+        upCommand (State("up 5:00 --color #ffffff --background #65a2ac --message hey hey", 0))
+
+    Assert.Equal(expected, actual)
+
+[<Fact>]
+let ``command 1`` () =
+    let expected =
+        Ok(
+            CommandAndOptions.Down(
+                TimeSpan(0, 5, 0),
+                [ Options.Color "#ffffff"
+                  Options.Background "#65a2ac"
+                  Options.Message "hey he" ]
+            ),
+            State("down 5:00 --color #ffffff --background #65a2ac --message hey hey", 63)
+        )
+
+    let actual =
+        command (State("down 5:00 --color #ffffff --background #65a2ac --message hey hey", 0))
+
+    Assert.Equal(expected, actual)
+
+[<Fact>]
+let ``command 2`` () =
+    let expected =
+        Ok(
+            CommandAndOptions.Up(
+                TimeSpan(0, 5, 0),
+                [ Options.Color "#ffffff"
+                  Options.Background "#65a2ac"
+                  Options.Message "hey he" ]
+            ),
+            State("up 5:00 --color #ffffff --background #65a2ac --message hey hey", 61)
+        )
+
+    let actual =
+        command (State("up 5:00 --color #ffffff --background #65a2ac --message hey hey", 0))
+
+    Assert.Equal(expected, actual)
